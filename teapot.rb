ad0dcfb6da64cc0508b71916b30e1050c82ecea7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2017-07-08 03:48:46 +1200

    Revert incorrect ordering.

--- teapot.rb
+++ teapot.rb
@@ -58,7 +58,7 @@ define_target "generate-template" do |target|
 						text = substitutions.apply(text)
 					end
 					
-					merged = Build::Text::Merge::combine(text.lines, arguments[:destination_path].read.lines)
+					merged = Build::Text::Merge::combine(arguments[:destination_path].read.lines, text.lines)
 					
 					write(arguments[:destination_path], merged.join)
 				else
